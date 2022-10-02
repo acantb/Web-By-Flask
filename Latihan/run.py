792560bb6543adbe8ce1870b1bd0861d5ebeb885
Acantb <114839565+acantb@users.noreply.github.com>
2022-10-02 14:02:33 +0900

    Add files via upload

---
+++ Latihan/run.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+
+data_mahasiswa=[
+    {
+        'nama: ', 'gamariah mandar',
+        'alamat :', 'kampis'
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/data mahasiswa")
+def data_mahasiswa():
+    return render_template("mahasiswa.html", data_mahasiswa=data_mahasiswa)
+
+if __name__=="__main__":
+    app.run(debug=True)
